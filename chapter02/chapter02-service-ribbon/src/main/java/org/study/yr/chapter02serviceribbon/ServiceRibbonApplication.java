53b646f2eb34c0b89fa770da3317a69c0b20c916
rongrongo0o0 <ryangxx@gmail.com>
2020-07-15 02:35:39 +0800

    git ignore

--- chapter02/chapter02-service-ribbon/src/main/java/org/study/yr/chapter02serviceribbon/ServiceRibbonApplication.java
+++ chapter02/chapter02-service-ribbon/src/main/java/org/study/yr/chapter02serviceribbon/ServiceRibbonApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceRibbonApplication {
 
